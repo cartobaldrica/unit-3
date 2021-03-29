272984b48f52aedc63856f6c38fb5f8b8817963f
cartobaldrica <garethfranklin@comcast.net>
2021-03-29 13:26:18 -0500

    Activity 9

--- js/main.js
+++ js/main.js
@@ -1,136 +1,53 @@
-window.onload = function(){
+window.onload = setMap();
+
+function setMap(){
+    
     var w = window.innerWidth, h = window.innerHeight; 
 
-    var cityPop = [
-        { 
-            city: 'Madison',
-            population: 233209
-        },
-        {
-            city: 'Milwaukee',
-            population: 594833
-        },
-        {
-            city: 'Green Bay',
-            population: 104057
-        },
-        {
-            city: 'Superior',
-            population: 27244
-        }
-    ];
-    
-    var container = d3.select("body")
+    var map = d3.select("body")
         .append("svg")
-        .attr("width",w)
-        .attr("height",h)
-        .attr("class","container")
-        .style("background-color","rgba(0,0,0,0.2");
+        .attr("class", "map")
+        .attr("width", w)
+        .attr("height", h);
+    //transverse cylindrical equal area projection, centered on Milwaukee
+    var projection = d3.geoEquirectangular()
+        .center([44.5, 0])
+        .rotate([87.95, -87.55, 90])
+        .angle(-90)
+        .scale(140000.00)
+        .translate([w / 2, h / 2]);
+
+    var path = d3.geoPath()
+        .projection(projection);
     
-    var innerRect = container.append("rect")
-        .attr("width",function(d){
-            return w - 100;
-        })
-        .attr("height",function(d){
-            return h - 100;
-        })
-        .attr("class","innerRect")
-        .attr("x", 50)
-        .attr("y", 50);
+    var promises = [d3.csv("data/MKE_neighborhoods_attributes.csv"),
+                    d3.json("data/mkeNeighborhoods.topojson"),
+                    d3.json("data/WI_counties.topojson")];
 
-    var minPop = d3.min(cityPop,function(d){
-        return d.population;
-    });
+    Promise.all(promises).then(callback)
 
-    var maxPop = d3.max(cityPop,function(d){
-        return d.population;
-    });
+    function callback(data){
+        var attributesData = data[0],
+            neighborhoodData = data[1],
+            countiesData = data[2];
 
-    var x = d3.scaleLinear()
-    .range([90,w-160]) //output min/max
-    .domain([0,3]); //input min/max
-
-    var y = d3.scaleLinear()
-        .range([h-50,50])
-        .domain([
-            0,
-            700000
-        ]);
-
-    var color = d3.scaleLinear()
-        .range([
-            "#fde0dd",
-            "#c51b8a"
-        ])
-        .domain([
-            minPop,
-            maxPop
-        ])
-
-    var circles = container.selectAll(".circles")
-        .data(cityPop)
-        .enter()
-        .append("circle")
-        .attr("class","circles")
-        .attr("r",function(d,i){
-            var area = d.population * 0.01;
-            return Math.sqrt(area/Math.PI);
-        })
-        .attr("cx",function(d,i){
-            return x(i);
-        })
-        .attr("cy",function(d){
-            return y(d.population)
-        })
-        .style("fill",function(d){
-            return color(d.population)
-        })
-        .style("stroke","#000");
-
-    var yAxis = d3.axisLeft(y);
-
-    var axis = container.append("g")
-        .attr("class","axis")
-        .attr("transform", "translate(50, 0)")
-        .call(yAxis);
-
-    yAxis(axis);
-
-    var title = container.append("text")
-        .attr("class", "title")
-        .attr("text-anchor", "middle")
-        .attr("x", w/2)
-        .attr("y", 30)
-        .text("City Populations");
-
-    var labels = container.selectAll(".labels")
-        .data(cityPop)
-        .enter()
-        .append("text")
-        .attr("class","labels")
-        .attr("text-anchor","left")
-        .attr("y", function(d){
-            return y(d.population);
-        });
-
-    var nameLine = labels.append("tspan")
-        .attr("class","nameLine")
-        .attr("x",function(d,i){
-            return x(i) + Math.sqrt((d.population * 0.01) / Math.PI) + 5;
-        })
-        .text(function(d){
-            return d.city;
-        })
-        .style("font-weight","bold");
-
-    var popLine = labels.append("tspan")
-        .attr("class","nameLine")
-        .attr("x",function(d,i){
-            return x(i) + Math.sqrt((d.population * 0.01) / Math.PI) + 5;
-        })
-        //dy is relative coordinate
-        .attr("dy", "15")
-        .text(function(d){
-            return "Pop. " + d3.format(",")(d.population);
-        });
+        var neighborhoodsObj = topojson.feature(neighborhoodData, neighborhoodData.objects.mkeNeighborhoods),
+            countiesObj = topojson.feature(countiesData, countiesData.objects.wiCounties);
+        //wisconsin counties
+        var counties = map.append("path")
+            .datum(countiesObj)
+            .attr("class", "counties")
+            .attr("d", path);
+        //milwaukee neighborhoods
+        var neighborhoods = map.selectAll(".neighborhoods")
+            .data(neighborhoodsObj.features)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                console.log(d)
+                return "neighborhood " + d.properties.NEIGHBORHD;
+            })
+            .attr("d", path);
+    }
 }
+
